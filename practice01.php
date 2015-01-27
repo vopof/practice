325dfbf2cf225c9afb921824cb92257a02945e54
vopof <vopof@a-ir.net>
2015-01-27 11:10:02 +0800

    Create practice01.php

---
+++ practice01.php
@@ -0,0 +1,28 @@
+
+<html lang="ja">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title> 練習01 </title>
+</head>
+<body>
+
+<?php
+
+$sam01 = 1;
+$sam02 = 2;
+
+if ($sam01 == $sam02) {
+
+echo"同じです";
+
+}
+else {
+
+echo"違います";
+
+}
+
+
+?>
+</body>
