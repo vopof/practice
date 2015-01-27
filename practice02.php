59481fe1772b5f272f85d01be33431feb44aa2f3
vopof <vopof@a-ir.net>
2015-01-27 11:10:43 +0800

    Create practice02.php

---
+++ practice02.php
@@ -0,0 +1,23 @@
+
+<html lang="ja">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title> 練習02 </title>
+</head>
+<body>
+
+<?php
+    // ループさせる配列を作って下さい。中身は数値で1, 2, 3, 4, 5でOKです。
+    $array = array(1, 2, 3, 4, 5);
+    // for文でループさせて、順番にデータを出力して下さい。
+    for ($i = 0; $i < count($array); $i++) {
+        echo $array[$i];
+        echo "<br>";
+    }
+?>
+
+
+
+?>
+</body>
